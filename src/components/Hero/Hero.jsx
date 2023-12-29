8c8fd10ba48ae784816e213f57f6341a8ab25cbf
robel tadesse <robeltadesse37@gmail.com>
2023-12-30 09:54:48 +1200

    left side of hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,6 +7,44 @@ function Hero() {
     <div className='hero'>
         <div className="Left-h">
             <Header />
+            <div className="the-best-ad">
+                <div></div>
+                <span>The best fitness club in town</span>
+            </div>
+            <div className="hero-text">
+                <div>
+                    <span className='stroke-text'>Shape </span>
+                    <span>Your</span>
+                </div>
+                <div>
+                    <span>
+                        Ideal body
+                    </span>
+                </div>
+                <div >
+                    <span>
+                        In here we will help you to Shape and build your ideal body and live up to your life to fullest
+                    </span> 
+                </div>
+            </div>
+            <div className="figures">
+                <div>
+                    <span>+140</span>
+                    <span>expert coaches</span>
+                </div>
+                <div>
+                    <span>+420</span>
+                    <span>member joined</span>
+                </div>
+                <div>
+                    <span>+40</span>
+                    <span>fittness Programs</span>
+                </div>
+            </div>
+            <div className="hero-buttons">
+                <button className="btn">Get Started</button>
+                <button className="btn">Learn more</button>
+            </div>
         </div>
         <div className="Right-h">Right side</div>
     </div>
